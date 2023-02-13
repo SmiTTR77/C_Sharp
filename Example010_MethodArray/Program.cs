c505b70e215fae51a6a9615a98c31f48078efb4f
Vladimir Smiryagin <smitt.r77@yandex.ru>
2023-02-13 23:04:53 +0300

    add Example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 10, 20, 3, 5, 6, 77, 9, 546, 3, 42, 2, 45 };
+int n = array.Length;
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
